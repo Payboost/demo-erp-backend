90e1451c745217bc4b449af98e2244842b780608
boost zone technologies <144770833+Payboost@users.noreply.github.com>
2025-01-31 16:20:48 +0500

    Delete routes/auth.js

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const { connectToDatabase, sql } = require('../db');
-const router = express.Router();
-require('dotenv').config();
-
-
-module.exports = router;
